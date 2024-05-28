53edb38a5a79a8730ba49c03a5da8992a669bab7
silverboi <dgplatac@icloud.com>
2024-05-28 00:53:29 -0600

    feat: Update pitching_stats.py script
    - Add functionality to retrieve pitching statistics for a player in a given season.
    - Include a function to retrieve team ID by brief name from MLB_TEAMS_INFO constant.
    - Update the main block to prompt for player name and team, set start and end dates dynamically, and download pitcher videos for the specified player and team.

--- pitching_stats_getter.py
+++ pitching_stats_getter.py
@@ -7,6 +7,9 @@ This script demonstrates how to use the pybaseball library to retrieve pitching 
 import os
 from pybaseball import playerid_lookup, statcast_pitcher
 from video_scraper.dowloader import download_pitcher_videos
+from datetime import datetime, timedelta
+from constants import MLB_TEAMS_INFO
+import pandas as pd
 
 
 # Function to retrieve pitching statistics for a player in a given season
@@ -36,23 +39,35 @@ def get_pitching_stats(player_name, start_date, end_date):
 
     return pitching_stats, unique_game_pks
 
-
+def get_team_id_by_brief_name(brief_name):
+    for team in MLB_TEAMS_INFO:
+        if team["Brief Name"] == brief_name:
+            return team["Team ID"]
+    return None
 
 
 if __name__ == "__main__":
     # Input for player name
     player_name = input("Enter the player's full name (e.g., Marcus Stroman): ")
+    player_team_brief_name = input("Enter the player's team (e.g., Cubs): ")
 
-    # Set the start and end dates for the 2023 MLB season
-    start_date = '2023-03-01'
-    end_date = '2023-09-30'  # Adjust the end date as needed
+    # Set the start and end dates for the MLB season
+    # start_date = '2024-03-01'
+    # end_date = '2024-05-20'  # Adjust the end date as needed
+    end_date = datetime.now()
+    start_date = end_date - timedelta(days=7)
+    # Format the dates as strings
+    start_date_str = start_date.strftime('%Y-%m-%d')
+    end_date_str = end_date.strftime('%Y-%m-%d')
 
     # Retrieve pitching statistics for the specified player and get unique game_pk values
-    pitching_stats, unique_game_pks = get_pitching_stats(player_name, start_date, end_date)
+    pitching_stats, unique_game_pks = get_pitching_stats(player_name, start_date_str, end_date_str)
+    # pitching_stats.to_csv('pitching_stats_assad.csv')
 
     video_folder = os.path.join("videos", player_name)
     os.makedirs(video_folder, exist_ok=True)
-    team_id = 112
+    #team_id = 112
+    team_id = get_team_id_by_brief_name(player_team_brief_name)
     # for game_pk in unique_game_pks:
     #     download_pitcher_videos(game_pk, player_name, video_folder, team_id)
     download_count = 0
